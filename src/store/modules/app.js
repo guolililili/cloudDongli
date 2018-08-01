52f69686533be91ad8df3f4b133186811e194360
guolililili <1322760015@qq.com>
2018-08-01 22:05:58 +0800

    code 整理

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,5 +1,4 @@
 import {otherRouter, appRouter} from '@/router/router';
-// import {otherRouter} from '@/router/index';
 import Util from '@/libs/util';
 import Cookies from 'js-cookie';
 import Vue from 'vue';
@@ -7,24 +6,12 @@ import Vue from 'vue';
 const app = {
     state: {
         cachePage: [],
-        lang: '',
-        isFullScreen: false,
-        openedSubmenuArr: [], // 要展开的菜单数组
-        menuTheme: 'dark', // 主题
-        themeColor: '',
         pageOpenedList: [{
             title: '首页',
             path: '',
             name: 'home'
         }],
         currentPageName: '',
-        currentPath: [
-            {
-                title: '首页',
-                path: '',
-                name: 'home'
-            }
-        ], // 面包屑数组
         menuList: [],
         routers: [
             otherRouter,
@@ -32,7 +19,6 @@ const app = {
         ],
         tagsList: [...otherRouter.children],
         tagsList:[],
-        messageCount: 0,
         dontCache: ['text-editor', 'artical-publish'] // 在这里定义你不想要缓存的页面的name属性值(参见路由配置router.js)
     },
     mutations: {
@@ -89,43 +75,10 @@ const app = {
             });
             state.menuList = menuList;
         },
-        changeMenuTheme (state, theme) {
-            state.menuTheme = theme;
-        },
-        changeMainTheme (state, mainTheme) {
-            state.themeColor = mainTheme;
-        },
-        addOpenSubmenu (state, name) {
-            let hasThisName = false;
-            let isEmpty = false;
-            if (name.length === 0) {
-                isEmpty = true;
-            }
-            if (state.openedSubmenuArr.indexOf(name) > -1) {
-                hasThisName = true;
-            }
-            if (!hasThisName && !isEmpty) {
-                state.openedSubmenuArr.push(name);
-            }
-        },
-        closePage (state, name) {
-            state.cachePage.forEach((item, index) => {
-                if (item === name) {
-                    state.cachePage.splice(index, 1);
-                }
-            });
-        },
         initCachepage (state) {
             if (localStorage.cachePage) {
                 state.cachePage = JSON.parse(localStorage.cachePage);
             }
-        },
-        removeTag (state, name) {
-            state.pageOpenedList.map((item, index) => {
-                if (item.name === name) {
-                    state.pageOpenedList.splice(index, 1);
-                }
-            });
         },
         pageOpenedList (state, get) {
             let openedPage = state.pageOpenedList[get.index];
@@ -138,52 +91,14 @@ const app = {
             state.pageOpenedList.splice(get.index, 1, openedPage);
             localStorage.pageOpenedList = JSON.stringify(state.pageOpenedList);
         },
-        clearAllTags (state) {
-            state.pageOpenedList.splice(1);
-            state.cachePage.length = 0;
-            localStorage.pageOpenedList = JSON.stringify(state.pageOpenedList);
-        },
-        clearOtherTags (state, vm) {
-            let currentName = vm.$route.name;
-            let currentIndex = 0;
-            state.pageOpenedList.forEach((item, index) => {
-                if (item.name === currentName) {
-                    currentIndex = index;
-                }
-            });
-            if (currentIndex === 0) {
-                state.pageOpenedList.splice(1);
-            } else {
-                state.pageOpenedList.splice(currentIndex + 1);
-                state.pageOpenedList.splice(1, currentIndex - 1);
-            }
-            let newCachepage = state.cachePage.filter(item => {
-                return item === currentName;
-            });
-            state.cachePage = newCachepage;
-            localStorage.pageOpenedList = JSON.stringify(state.pageOpenedList);
-        },
         setOpenedList (state) {
             state.pageOpenedList = localStorage.pageOpenedList ? JSON.parse(localStorage.pageOpenedList) : [otherRouter.children[0]];
-        },
-        setCurrentPath (state, pathArr) {
-            state.currentPath = pathArr;
         },
         setCurrentPageName (state, name) {
             state.currentPageName = name;
         },
         setAvator (state, path) {
             localStorage.avatorImgPath = path;
-        },
-        switchLang (state, lang) {
-            state.lang = lang;
-            Vue.config.lang = lang;
-        },
-        clearOpenedSubmenu (state) {
-            state.openedSubmenuArr.length = 0;
-        },
-        setMessageCount (state, count) {
-            state.messageCount = count;
         },
         increateTag (state, tagObj) {
             if (!Util.oneOf(tagObj.name, state.dontCache)) {
--- src/views/own-space/own-space.vue
+++ src/views/own-space/own-space.vue
@@ -15,35 +15,11 @@
                     :model="userForm" 
                     :label-width="100" 
                     label-position="right"
-                    :rules="inforValidate"
                 >
                     <FormItem label="用户姓名：" prop="name">
                         <div style="display:inline-block;width:300px;">
                             <Input v-model="userForm.name" ></Input>
                         </div>
-                    </FormItem>
-                    <FormItem label="用户手机：" prop="cellphone" >
-                        <div style="display:inline-block;width:204px;">
-                            <Input v-model="userForm.cellphone" @on-keydown="hasChangePhone"></Input>
-                        </div>
-                        <div style="display:inline-block;position:relative;">
-                            <Button @click="getIdentifyCode" :disabled="canGetIdentifyCode">{{ gettingIdentifyCodeBtnContent }}</Button>
-                            <div class="own-space-input-identifycode-con" v-if="inputCodeVisible">
-                                <div style="background-color:white;z-index:110;margin:10px;">
-                                    <Input v-model="securityCode" placeholder="请填写短信验证码" ></Input>
-                                    <div style="margin-top:10px;text-align:right">
-                                        <Button type="ghost" @click="cancelInputCodeBox">取消</Button>
-                                        <Button type="primary" @click="submitCode" :loading="checkIdentifyCodeLoading">确定</Button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </FormItem>
-                    <FormItem label="公司：">
-                        <span>{{ userForm.company }}</span>
-                    </FormItem>
-                    <FormItem label="部门：">
-                        <span>{{ userForm.department }}</span>
                     </FormItem>
                     <FormItem label="登录密码：">
                         <Button type="text" size="small" @click="showEditPassword">修改密码</Button>
@@ -80,14 +56,6 @@
 export default {
     name: 'ownspace',
     data () {
-        const validePhone = (rule, value, callback) => {
-            var re = /^1[0-9]{10}$/;
-            if (!re.test(value)) {
-                callback(new Error('请输入正确格式的手机号'));
-            } else {
-                callback();
-            }
-        };
         const valideRePassword = (rule, value, callback) => {
             if (value !== this.editPasswordForm.newPass) {
                 callback(new Error('两次输入密码不一致'));
@@ -97,32 +65,13 @@ export default {
         };
         return {
             userForm: {
-                name: '',
-                cellphone: '',
-                company: '',
-                department: ''
+                name: ''
             },
             uid: '', // 登录用户的userId
-            securityCode: '', // 验证码
-            phoneHasChanged: false, // 是否编辑了手机
             save_loading: false,
-            identifyError: '', // 验证码错误
             editPasswordModal: false, // 修改密码模态框显示
             savePassLoading: false,
             oldPassError: '',
-            identifyCodeRight: false, // 验证码是否正确
-            hasGetIdentifyCode: false, // 是否点了获取验证码
-            canGetIdentifyCode: false, // 是否可点获取验证码
-            checkIdentifyCodeLoading: false,
-            inforValidate: {
-                name: [
-                    { required: true, message: '请输入姓名', trigger: 'blur' }
-                ],
-                cellphone: [
-                    { required: true, message: '请输入手机号码' },
-                    { validator: validePhone }
-                ]
-            },
             editPasswordForm: {
                 oldPass: '',
                 newPass: '',
@@ -141,39 +90,14 @@ export default {
                     { required: true, message: '请再次输入新密码', trigger: 'blur' },
                     { validator: valideRePassword, trigger: 'blur' }
                 ]
-            },
-            inputCodeVisible: false, // 显示填写验证码box
-            initPhone: '',
-            gettingIdentifyCodeBtnContent: '获取验证码' // “获取验证码”按钮的文字
+            }
         };
     },
     methods: {
-        getIdentifyCode () {
-            this.hasGetIdentifyCode = true;
-            this.$refs['userForm'].validate((valid) => {
-                if (valid) {
-                    this.canGetIdentifyCode = true;
-                    let timeLast = 60;
-                    let timer = setInterval(() => {
-                        if (timeLast >= 0) {
-                            this.gettingIdentifyCodeBtnContent = timeLast + '秒后重试';
-                            timeLast -= 1;
-                        } else {
-                            clearInterval(timer);
-                            this.gettingIdentifyCodeBtnContent = '获取验证码';
-                            this.canGetIdentifyCode = false;
-                        }
-                    }, 1000);
-                    this.inputCodeVisible = true;
-                    // you can write ajax request here
-                }
-            });
-        },
         showEditPassword () {
             this.editPasswordModal = true;
         },
         cancelEditUserInfor () {
-            this.$store.commit('removeTag', 'ownspace_index');
             localStorage.pageOpenedList = JSON.stringify(this.$store.state.app.pageOpenedList);
             let lastPageName = '';
             if (this.$store.state.app.pageOpenedList.length > 1) {
@@ -188,20 +112,8 @@ export default {
         saveEdit () {
             this.$refs['userForm'].validate((valid) => {
                 if (valid) {
-                    if (this.phoneHasChanged && this.userForm.cellphone !== this.initPhone) { // 手机号码修改过了而且修改之后的手机号和原来的不一样
-                        if (this.hasGetIdentifyCode) { // 判断是否点了获取验证码
-                            if (this.identifyCodeRight) { // 判断验证码是否正确
-                                this.saveInfoAjax();
-                            } else {
-                                this.$Message.error('验证码错误，请重新输入');
-                            }
-                        } else {
-                            this.$Message.warning('请先点击获取验证码');
-                        }
-                    } else {
                         this.saveInfoAjax();
                     }
-                }
             });
         },
         cancelEditPass () {
@@ -231,33 +143,11 @@ export default {
             });
         },
         init () {
-            this.userForm.name = 'Lison';
-            this.userForm.cellphone = '17712345678';
-            this.initPhone = '17712345678';
-            this.userForm.company = 'TalkingData';
-            this.userForm.department = '可视化部门';
+            this.userForm.name = 'ken';
         },
         cancelInputCodeBox () {
             this.inputCodeVisible = false;
             this.userForm.cellphone = this.initPhone;
-        },
-        submitCode () {
-            let vm = this;
-            vm.checkIdentifyCodeLoading = true;
-            if (this.securityCode.length === 0) {
-                this.$Message.error('请填写短信验证码');
-            } else {
-                setTimeout(() => {
-                    this.$Message.success('验证码正确');
-                    this.inputCodeVisible = false;
-                    this.checkIdentifyCodeLoading = false;
-                }, 1000);
-            }
-        },
-        hasChangePhone () {
-            this.phoneHasChanged = true;
-            this.hasGetIdentifyCode = false;
-            this.identifyCodeRight = false;
         },
         saveInfoAjax () {
             this.save_loading = true;
